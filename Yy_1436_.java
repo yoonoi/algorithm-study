c0f4a8a1bf7606740db7456bbb3a5ee4ef8a7949
Yoonoi <dbddbd39@naver.com>
2023-08-09 23:53:00 +0900

    Add / 유뇽

---
+++ Yy_1436_.java
@@ -0,0 +1,26 @@
+package bruteforce;
+
+import java.util.Scanner;
+
+/**
+ * https://st-lab.tistory.com/103
+ */
+public class Yy_1436_ {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for(int i=666, cnt = 0; ; i++) {
+			String str = String.valueOf(i);
+			
+			if(str.contains("666"))	cnt++;
+			
+			if(cnt == n) {
+				System.out.println(str);
+				break;
+			}
+		}
+	}
+}
