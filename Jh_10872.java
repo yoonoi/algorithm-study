412cd780a1bc358ef4ea05c0e5d57df5e62c033f
joo9837 <joo9837@naver.com>
2023-08-13 21:11:37 +0900

    Add / 주히

---
+++ Jh_10872.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Jh_10872 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int result=1;
+        for(int i=1; i<=n; i++){
+            result*=i;
+        }
+        System.out.println(result);
+    }
+}
