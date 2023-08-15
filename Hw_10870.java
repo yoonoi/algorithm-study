1a1e61240bb4d2390144bb5f602a2bbb5742f057
Yoonoi <dbddbd39@naver.com>
2023-08-15 19:47:04 +0900

    Mod / 브랜치

---
+++ Hw_10870.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Hw_10870 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println(fibonaci(sc.nextInt()));
+		
+	}
+	
+	public static int fibonaci(int n) {
+		if(n == 1) {
+			return 1;
+		}
+		else if(n  == 0) {
+			return 0;
+		}
+		return fibonaci(n - 1) + fibonaci(n - 2);
+		
+	}
+}
